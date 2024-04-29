dfa115960298f814df4e948ef1678c39e75d885a
Alex Sandro <79149058+alexsandrodummer@users.noreply.github.com>
2024-04-29 17:24:13 -0300

    Add files via upload

---
+++ mapa programação 2/exceptions/DadoConsultadoException.java
@@ -0,0 +1,10 @@
+package br.unicesumar.aula.exceptions;
+
+
+
+@SuppressWarnings("serial")
+public class DadoConsultadoException extends Exception{
+    public DadoConsultadoException(String message) {
+        super(message);
+    }
+}
